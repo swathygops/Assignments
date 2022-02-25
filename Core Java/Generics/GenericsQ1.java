7fc775e28a175f7e8366e64c817713230bcdfd2b
swathygops <99176938+swathygops@users.noreply.github.com>
2022-02-25 10:38:34 +0530

    Add files via upload

---
+++ Core Java/Generics/GenericsQ1.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+import java.util.Set;
+public class GenericsQ1 
+{
+	public static void main(String[] args) 
+	{
+		        Set<String> Employee = new HashSet<>();
+		        {
+		        Employee.add("Id-RA1811037010086");
+		        Employee.add("Name-Swathy Gopal N");
+		        Employee.add("Salary-30000");
+		        Employee.add("Department-Java Full Stack Angular Developer");
+		        System.out.println(Employee);
+		        
+		    }
+		    	}
+		       
+		        	
+
+	}
---
+++ Core Java/Generics/GenericsQ2.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+public class GenericsQ2 {
+
+ public static void main(String[] args)
+ {
+
+     HashMap<Integer,Double> map = new HashMap<Integer,Double>();
+
+     map.put(10, 10.1);
+     map.put(20, 20.2);
+     map.put(30, 30.3);
+     map.put(40, 40.4);
+     map.put(50, 50.5);
+     map.put(60, 60.6);
+     map.put(70, 70.7);
+     map.put(80, 80.8);
+     map.put(90, 90.9);
+     map.put(100, 100.10);
+
+
+     System.out.println(map);
+
+    
+     }
+ }
