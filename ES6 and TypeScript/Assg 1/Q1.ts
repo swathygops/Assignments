3f4efc81c031ae87752df385fefb0ba265a894de
swathygops <99176938+swathygops@users.noreply.github.com>
2022-04-06 10:19:23 +0530

    Assg1

---
+++ ES6 and TypeScript/Assg 1/Q1.ts
@@ -0,0 +1,13 @@
+let number = 40;
+try {
+  number = 99;
+} catch (err) {
+  console.log(err);
+}
+console.log(number);
+const obj={
+    name:"Sara"
+};
+console.log(obj.name);
+obj.name="Sarah";
+console.log(obj.name);
