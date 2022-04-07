497113a72c20cdced6ddb68e64783a3ab66d535f
swathygops <99176938+swathygops@users.noreply.github.com>
2022-04-07 16:30:32 +0530

    Q4

---
+++ JUnit 5/Q4/junitQ4.java
@@ -0,0 +1,11 @@
+package workjunit;
+
+public class junitQ4 {
+    public static int multi(int a, int b){
+        return a * b;
+    }
+    public static int div(int a, int b){
+        return a / b;
+    }
+
+}
